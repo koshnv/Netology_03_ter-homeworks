f8e69d4b4d82524cb92b8c3720d06411e7709bd5
Konstantin Shcheglov <konstantin.scheglov@belka.ai>
2025-05-04 17:18:18 +0500

    commit init

---
+++ src/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  #vm_web_name = "${var.vm_web_name}"
+  #vm_db_name  = "${var.vm_db_name}"
+  #develop_db  = "${var.vpc_name}-${var.environment}" #  Формирует имя подсети (например, "develop-db")
+  #full_metadata  = merge(var.metadata, { ssh-keys = var.ssh-keys_pub }) # Для динамической сборки metadata
+}
---
+++ src/variables.tf
@@ -0,0 +1,50 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network&subnet name"
+}
+
+
+###ssh vars
+variable "ssh-keys_pub" {
+  type        = string
+  default     = null
+  description = "Public SSH key for VM access"
+}
+
+###metadata for VMs
+variable "metadata" {
+  type = map(string)
+  default = {
+    serial-port-enable = "1"
+    ssh-keys           = null
+  }
+  description = "Metadata for VMs"
+}
